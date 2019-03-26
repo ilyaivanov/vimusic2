983bbbe068854094a8c44cbf32832b7f6f1ab2a5
Ilya <illiai@wix.com>
2019-03-26 17:38:28 +0200

    Updates

---
+++ precache-manifest.f4fc8197a4b5e04ec431ee8ed09d331b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dcc27a44f91c981e2ea",
+    "url": "/vimusic2/static/js/runtime~main.3a21c012.js"
+  },
+  {
+    "revision": "93b80ffc4544400152c8",
+    "url": "/vimusic2/static/js/main.dc052707.chunk.js"
+  },
+  {
+    "revision": "0e6ca656114bb3c77457",
+    "url": "/vimusic2/static/js/2.1b58a2ad.chunk.js"
+  },
+  {
+    "revision": "93b80ffc4544400152c8",
+    "url": "/vimusic2/static/css/main.1e65dcca.chunk.css"
+  },
+  {
+    "revision": "09d21caf75e73cf384a69f67bc9395b8",
+    "url": "/vimusic2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Vimusic2/precache-manifest.55a3f2062c42cb2651943b9b4f4a8692.js"
+  "/vimusic2/precache-manifest.f4fc8197a4b5e04ec431ee8ed09d331b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Vimusic2/index.html", {
+workbox.routing.registerNavigationRoute("/vimusic2/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
